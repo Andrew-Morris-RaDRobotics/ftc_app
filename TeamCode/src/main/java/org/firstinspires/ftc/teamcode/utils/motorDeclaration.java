0b8f88802fb6801f6aa1659536d4c519e70c7d01
Andrew <americanandrew2022@gmail.com>
2017-11-25 09:44:50 -0800

    added motorDeclaration (the real stuff)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/motorDeclaration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/motorDeclaration.java
@@ -1,8 +1,43 @@
 package org.firstinspires.ftc.teamcode.utils;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 /**
- * Created by Andrew on 11/24/2017.
+ * Created by DeathChicken on 11/24/2017.
  */
 
 public class motorDeclaration {
+
+    public DcMotor fl;
+    public DcMotor fr;
+    public DcMotor br;
+    public DcMotor bl;
+
+    public motorDeclaration(HardwareMap hardwareMap) {
+
+        fr = hardwareMap.dcMotor.get("fr");
+        fl = hardwareMap.dcMotor.get("fl");
+        bl = hardwareMap.dcMotor.get("bl");
+        br = hardwareMap.dcMotor.get("br");
+
+
+
+    }
+
+//    public void setP(double powerF, double powerS){
+//        fr.setPower(-powerF + powerS);
+//        fl.setPower(powerF+powerS);
+//        br.setPower(-powerF -powerS);
+//        bl.setPower(powerF-powerS);
+//    }
+    public void setP(double powerF, double powerS, double turn){
+        fr.setPower(-(-powerF + powerS+ turn));
+        fl.setPower(-(powerF+powerS+turn));
+        br.setPower(-(-powerF -powerS+turn));
+        bl.setPower(-(powerF-powerS+turn));
+    }
+
+
+
 }
