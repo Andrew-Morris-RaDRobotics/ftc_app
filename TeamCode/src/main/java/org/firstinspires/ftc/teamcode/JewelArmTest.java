14c412aede4ea918b686aa64ca771107ebfef104
Andrew <americanandrew2022@gmail.com>
2017-12-02 13:59:43 -0800

    Edited GlyphSortingTest, moved GlyphVision, added gyroTurn, nad added some motor stuff to JewelArmTest.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArmTest.java
@@ -4,8 +4,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.utils.gyroCompass;
 import org.firstinspires.ftc.teamcode.utils.motorDeclaration;
 
 @Autonomous (name= "JewelArmTest", group= "testing")
@@ -15,35 +17,145 @@ public class JewelArmTest extends LinearOpMode {
     public Servo jewelStick;
     public ColorSensor leftJewel;
     public ColorSensor rightJewel;
+    public DcMotor fl;
+    public DcMotor fr;
+    public DcMotor br;
+    public DcMotor bl;
     public motorDeclaration Motors;
-
+    public gyroCompass testGyro;
     @Override
     public void runOpMode() throws InterruptedException {
+        testGyro = new gyroCompass(hardwareMap);
+        Motors = new motorDeclaration(hardwareMap);
+        double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+        double DRIVE_GEAR_REDUCTION = 2.0;     // This is < 1.0 if geared UP
+        double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+        double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                (WHEEL_DIAMETER_INCHES * 3.1415);
+
+        fr = hardwareMap.dcMotor.get("fr");
+        fl = hardwareMap.dcMotor.get("fl");
+        bl = hardwareMap.dcMotor.get("bl");
+        br = hardwareMap.dcMotor.get("br");
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        telemetry.addData("Path0", "Starting at %7d :%7d",
+                fr.getCurrentPosition(),
+                bl.getCurrentPosition());
+        telemetry.update();
+
+
 
         jewelStick = hardwareMap.servo.get("jewelStick");
         leftJewel = hardwareMap.colorSensor.get("leftJewel");
         rightJewel = hardwareMap.colorSensor.get("rightJewel");
 
-        jewelStick.setPosition(0);
-        //runOpMode();
-        telemetry.addData("Left Red", leftJewel.red());
-        telemetry.addData("Left Blue", leftJewel.blue());
-        telemetry.addData("Right Red", rightJewel.red());
-        telemetry.addData("Right Blue", rightJewel.blue());
-        telemetry.update();
-        try {
-            Thread.sleep(100L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            if (leftJewel.red() > rightJewel.red() && rightJewel.blue() > leftJewel.blue()) {
-                telemetry.addData("left side is red", "right side is blue");
-            } else if (leftJewel.red() < rightJewel.red() && rightJewel.blue() < leftJewel.blue()) {
-                telemetry.addData("right side is red", "left side is blue");
-            } else {
-                telemetry.addData("No reading", "");
+
+        waitForStart();
+
+        jewelStick.setPosition(0.4);
+        int completed =  0;
+        int color = 0; //1 is red 2 is blu (left side)
+        double curr = 0.0;
+        int stage = 0;
+
+        while (opModeIsActive()) {
+            if(completed==0&&color==0){
+                telemetry.addData("Completed2",completed);
+                telemetry.addData("Left Red", leftJewel.red());
+                telemetry.addData("Left Blue", leftJewel.blue());
+                telemetry.addData("Right Red", rightJewel.red());
+                telemetry.addData("Right Blue", rightJewel.blue());
+                telemetry.addData("color:", color);
+                telemetry.update();
+
+                if (leftJewel.red() - 1 > rightJewel.red() && rightJewel.blue() - 1 > leftJewel.blue()) {
+                    telemetry.addData("left side is red", "right side is blue");
+                    color = 1;
+                    curr = testGyro.getHeading();
+                    sleep(1000);
+                } else if (leftJewel.red() + 1 < rightJewel.red() && rightJewel.blue() + 1 < leftJewel.blue()) {
+                    telemetry.addData("right side is red", "left side is blue");
+                    color = 2;
+                    curr = testGyro.getHeading();
+                    sleep(1000);
+
+                } else {
+                    telemetry.addData("No reading", "");
+                }
+
             }
-            jewelStick.setPosition(95.75);
-            //jewelStick.setPosition(180/191.5);
+            if(color==1) {
+                double target = curr+10;
+               // if(testGyro.getHeading()<target && stage==0) {
+                if(stage<2){
+                    Motors.setP(0, 0, (target-testGyro.getHeading())/5);
+               }
+                if(testGyro.getHeading()>=target && stage==0){
+                    target=curr;
+                    jewelStick.setPosition(0);
+                    stage++;
+                }
+                if(stage==1 && testGyro.getHeading()<=target+.5){
+                    stage++;
+                    Motors.setP(0,0,0);
+                }
+
+            }
+            if(color==2){
+                double target = curr-10;
+                //if(testGyro.getHeading()<target && stage==0) {
+                if(stage<2) {
+                    Motors.setP(0, 0, (target - testGyro.getHeading()) / 5);
+                } //}
+                if(testGyro.getHeading()<target && stage==0){
+                    target=curr;
+                    jewelStick.setPosition(0);
+                    stage++;
+                }
+                if(stage==1 && testGyro.getHeading()>target-.5){
+                    stage++;
+                    Motors.setP(0,0,0);
+                }
+
+            }
+
+        if(completed==2) {
+            int newLeftTarget = fr.getCurrentPosition() + (int) (12 * COUNTS_PER_INCH);
+            int newRightTarget = fr.getCurrentPosition() + (int) (12 * COUNTS_PER_INCH);
+            fr.setTargetPosition(newLeftTarget);
+            bl.setTargetPosition(newRightTarget);
+            fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+            telemetry.addData("Path2", "Running at %7d :%7d",
+                    fr.getCurrentPosition(),
+                    bl.getCurrentPosition());
+            telemetry.update();
+            fr.setPower(Math.abs(.3));
+            bl.setPower(Math.abs(.3));
+            if (completed == 0 && newLeftTarget >= fr.getCurrentPosition() - 4 && newLeftTarget <= fr.getCurrentPosition() + 4 && newRightTarget >= bl.getCurrentPosition() - 4 && newRightTarget <= bl.getCurrentPosition() + 4) {
+                completed++;
+                telemetry.addData("Completed1", "");
+                telemetry.update();
+            }
+        }
+
+        if(completed==1) {
+
+            fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            fr.setPower(0);
+            bl.setPower(0);
+            completed++;
+            telemetry.addData("Completed1",completed);
+            telemetry.update();
+        }
+
+
+
         }
 
     }/*
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/GlyphSortingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/GlyphSortingTest.java
@@ -1,77 +1,106 @@
 package org.firstinspires.ftc.teamcode.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-import java.util.concurrent.TimeUnit;
-
+@TeleOp(name = "GlyphSorting", group = "testing")
 
 public class GlyphSortingTest extends OpMode {
 
-    private ColorSensor colorSensor;
+    private static final int BROWN = 2;
+    private static final int GRAY = 1;
+    private static final int EMPTY = 0;
+    private int glyphState = 0;
+    private int[][] cryptobox = new int[4][3];
 
     @Override
     public void init() {
         ColorSensor colorSensor;
-        colorSensor = hardwareMap.colorSensor.get("colordistance1");
-        double ratio = colorSensor.blue() / colorSensor.red();
+        colorSensor = hardwareMap.colorSensor.get("glyphColor");
+        double blue = colorSensor.blue();
+        double red = colorSensor.red();
+        double ratio = blue / red;
         telemetry.addData("Ratio", ratio);
     }
 
     @Override
     public void loop() {
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        colorSensor = hardwareMap.colorSensor.get("colordistance1");
-        double ratio = colorSensor.blue() / colorSensor.red();
-        int[][] cryptobox = new int[4][3];
+        ColorSensor colorSensor = hardwareMap.colorSensor.get("glyphColor");
+        double blue = colorSensor.blue();
+        double red = colorSensor.red();
+        double ratio = blue / red;
 
-        if (ratio > 0.8) {
-            if (cryptobox[2][0] == 0) {
-                telemetry.addData("(0,2) Gray", "");
-                cryptobox[2][0] = 1;
-            } else if (cryptobox[0][0] == 0) {
-                telemetry.addData("(0,4) Gray", "");
-                cryptobox[0][0] = 1;
-            } else if (cryptobox[3][1] == 0) {
-                telemetry.addData("(1,1) Gray", "");
-                cryptobox[3][1] = 1;
-            } else if (cryptobox[1][1] == 0) {
-                telemetry.addData("(1,3) Gray", "");
-                cryptobox[1][1] = 1;
-            } else if (cryptobox[2][2] == 0) {
-                telemetry.addData("(2,2) Gray", "");
-                cryptobox[2][2] = 1;
-            } else if (cryptobox[0][2] == 0) {
-                telemetry.addData("(2,4) Gray", "");
-                cryptobox[0][2] = 1;
+        while (ratio > 0.5 && ratio < 0.9) {
+            glyphState = BROWN;
+        }
+        while (ratio > 0.8 && ratio < 0.9) {
+            glyphState = GRAY;
+        }
+
+        if (glyphState == GRAY) {
+            if (cryptobox[2][0] == EMPTY && cryptobox[3][0] != EMPTY) {
+                cryptobox[2][0] = GRAY;
+                glyphState = EMPTY;
+            } else if (cryptobox[0][0] == EMPTY && cryptobox[1][0] != EMPTY) {
+                cryptobox[0][0] = GRAY;
+                glyphState = EMPTY;
+            } else if (cryptobox[3][1] == EMPTY) {
+                cryptobox[3][1] = GRAY;
+                glyphState = EMPTY;
+            } else if (cryptobox[1][1] == EMPTY && cryptobox[2][1] != EMPTY) {
+                cryptobox[1][1] = GRAY;
+                glyphState = EMPTY;
+            } else if (cryptobox[2][2] == EMPTY && cryptobox[3][2] != EMPTY) {
+                cryptobox[2][2] = GRAY;
+                glyphState = EMPTY;
+            } else if (cryptobox[0][2] == EMPTY && cryptobox[1][2] != EMPTY) {
+                cryptobox[0][2] = GRAY;
+                glyphState = EMPTY;
             } else {
                 telemetry.addData("", "");
+                glyphState = EMPTY;
             }
-        } else if (ratio > 0.5 && ratio < 0.8) {
-            if (cryptobox[3][0] == 0) {
-                telemetry.addData("(0,0 Brown", "");
-                cryptobox[3][0] = 2;
-            } else if (cryptobox[1][0] == 0) {
-                telemetry.addData("(0,2) Brown", "");
-                cryptobox[1][0] = 2;
-            } else if (cryptobox[2][1] == 0) {
-                telemetry.addData("(1,2) Brown", "");
-                cryptobox[2][1] = 2;
-            } else if (cryptobox[0][1] == 0) {
-                telemetry.addData("(1,4) Brown", "");
-                cryptobox[0][1] = 2;
-            } else if (cryptobox[3][2] == 0) {
-                telemetry.addData("(2,0) Brown", "");
-                cryptobox[3][2] = 2;
-            } else if (cryptobox[1][2] == 0) {
-                telemetry.addData("(2,2) Brown", "");
-                cryptobox[1][2] = 2;
+
+        }
+        if (glyphState == BROWN) {
+            if (cryptobox[3][0] == EMPTY) {
+                cryptobox[3][0] = BROWN;
+                glyphState = EMPTY;
+            } else if (cryptobox[1][0] == EMPTY && cryptobox[2][0] != EMPTY) {
+                cryptobox[1][0] = BROWN;
+                glyphState = EMPTY;
+            } else if (cryptobox[2][1] == EMPTY && cryptobox[3][1] != EMPTY) {
+                cryptobox[2][1] = BROWN;
+                glyphState = EMPTY;
+            } else if (cryptobox[0][1] == EMPTY && cryptobox[1][1] != EMPTY) {
+                cryptobox[0][1] = BROWN;
+                glyphState = EMPTY;
+            } else if (cryptobox[3][2] == EMPTY) {
+                cryptobox[3][2] = BROWN;
+                glyphState = EMPTY;
+            } else if (cryptobox[1][2] == EMPTY && cryptobox[2][2] != EMPTY) {
+                cryptobox[1][2] = BROWN;
+                glyphState = EMPTY;
             }
         }
+        telemetry.addLine()
+                .addData("|", cryptobox[0][0])
+                .addData("|", cryptobox[0][1])
+                .addData("|", cryptobox[0][2]);
+        telemetry.addLine()
+                .addData("|", cryptobox[1][0])
+                .addData("|", cryptobox[1][1])
+                .addData("|", cryptobox[1][2]);
+        telemetry.addLine()
+                .addData("|", cryptobox[2][0])
+                .addData("|", cryptobox[2][1])
+                .addData("|", cryptobox[2][2]);
+        telemetry.addLine()
+                .addData("|", cryptobox[3][0])
+                .addData("|", cryptobox[3][1])
+                .addData("|", cryptobox[3][2]);
+        telemetry.update();
 
     }
 }
