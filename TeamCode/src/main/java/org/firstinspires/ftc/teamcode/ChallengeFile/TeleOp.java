9120a97dc4a5eabaf3452203f66cad4e36bbee70
Andrew <americanandrew2022@gmail.com>
2017-12-08 21:23:49 -0800

    Added extra intake and conveyor controls to TeleOp (it's now ready for competition!), changed JewelArmTest a bit.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChallengeFile/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChallengeFile/TeleOp.java
@@ -13,51 +13,110 @@ public class TeleOp extends OpMode {
     public DcMotor fr;
     public DcMotor br;
     public DcMotor bl;
-    private Servo leftIntakeFlipper;
-    private Servo rightIntakeFlipper;
-    private DcMotor intakeDrive;
-    private DcMotor fwopperDrive;
-    private Servo intakeBucket;
+     Servo leftIntakeFlipper;
+     Servo rightIntakeFlipper;
+    DcMotor intakeDrive;
+    DcMotor fwopperDrive;
+    DcMotor conveyor;
 
+    Servo intakeBucket;
+    Servo jewelStick;
+    int floppers;
+    double position;
+    int conveyorP;
     @Override
     public void init() {
+        jewelStick = hardwareMap.servo.get("jewelStick");
         fr = hardwareMap.dcMotor.get("fr");
         fl = hardwareMap.dcMotor.get("fl");
         bl = hardwareMap.dcMotor.get("bl");
         br = hardwareMap.dcMotor.get("br");
-        leftIntakeFlipper = hardwareMap.servo.get("leftIntakeFlipper");
-        rightIntakeFlipper = hardwareMap.servo.get("rightIntakeFlipper");
+      leftIntakeFlipper = hardwareMap.servo.get("leftIntakeFlipper");
+      rightIntakeFlipper = hardwareMap.servo.get("rightIntakeFlipper");
         intakeBucket = hardwareMap.servo.get("intakeBucket");
         intakeDrive = hardwareMap.dcMotor.get("intakeDrive");
         fwopperDrive = hardwareMap.dcMotor.get("fwopperDrive");
+        conveyor = hardwareMap.dcMotor.get("conveyer");
+        conveyorP=0;
+        floppers = 0;
+        position=0.0;
     }
+
     @Override
     public void loop() {
-        double speed = 0.25;
-        speed = speed + gamepad1.right_trigger*0.5;
+        double speed = 0.35;
+        double speed2 = 0.35;
+        speed = speed + gamepad1.right_trigger * 0.65;
+        speed2 = speed2 + gamepad2.right_trigger * 0.65;
 
-
-        System.out.println(gamepad1.right_bumper);
-        double frSpeed = (speed)*(-gamepad1.left_stick_y +gamepad1.left_stick_x -gamepad1.right_stick_x);
-        double flSpeed = (speed)*(+gamepad1.left_stick_y +gamepad1.left_stick_x -gamepad1.right_stick_x);
-        double brSpeed = (speed)*(-gamepad1.left_stick_y -gamepad1.left_stick_x -gamepad1.right_stick_x);
-        double blSpeed = (speed)*(+gamepad1.left_stick_y -gamepad1.left_stick_x -gamepad1.right_stick_x);
-
+        //System.out.println(gamepad1.right_bumper);
+        double frSpeed = (speed) * -(-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
+        double flSpeed = (speed) * -(+gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
+        double brSpeed = (speed) * -(-gamepad1.left_stick_y  + gamepad1.left_stick_x - gamepad1.right_stick_x);
+        double blSpeed = (speed) * -(+gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x);
         fr.setPower(frSpeed);
         fl.setPower(flSpeed);
         br.setPower(brSpeed);
         bl.setPower(blSpeed);
 
-        boolean fwopperSpeedForward = gamepad1.dpad_up;
-        boolean fwopperSpeedBack = gamepad1.dpad_down;
-        if (fwopperSpeedForward) {
-            fwopperDrive.setPower(0.2);
-        } else if (fwopperSpeedBack) {
-            fwopperDrive.setPower(-0.2);
+//conveyor
+        if (gamepad2.dpad_down) {
+            conveyorP=1;
+        } else if (gamepad2.dpad_up) {
+            conveyorP=-1;
+        }
+        else if(gamepad2.dpad_left||gamepad2.dpad_right){
+            conveyorP=0;
         }
 
-        double intakeSpeed = gamepad1.left_trigger-gamepad1.right_trigger;
-        intakeDrive.setPower(intakeSpeed);
+        if(conveyorP==1){
+            conveyor.setPower(speed2-.1);
+        }
+        else if(conveyorP==-1){
+            conveyor.setPower(-speed2+.1);
+        }
+        else{
+            conveyor.setPower(0);
+        }
+
+        //floppers
+        if(gamepad2.a){
+            floppers = 1;
+        }
+        else if(gamepad2.b){
+            floppers=0;
+        }
+        else if(gamepad2.x){
+            floppers=-1;
+        }
+        if(floppers==1){
+            intakeDrive.setPower(speed2*2);
+        }
+        else if(floppers==-1){
+            intakeDrive.setPower(-speed2*2);
+        }
+        else{
+            intakeDrive.setPower(0);
+        }
+
+
+        fwopperDrive.setPower(speed2*gamepad2.left_stick_y);
+
+        position=position+(gamepad2.right_stick_y*.01);
+        if(position<0){
+            position=0;
+        }
+        if(position>1){
+            position=1;
+        }
+        leftIntakeFlipper.setPosition(position);
+        rightIntakeFlipper.setPosition(position);
+        telemetry.addData("pos",position);
+        telemetry.update();
+        intakeBucket.setPosition(.68-(gamepad2.left_trigger*.5));
+        jewelStick.setPosition(.5+gamepad1.left_trigger);
+//        double intakeSpeed = gamepad1.left_trigger-gamepad1.right_trigger;
+//        intakeDrive.setPower(intakeSpeed);
 
         // Code using the second gamepad (for the operator/robot controller).
 //        double fwopperSpeed = gamepad2.left_stick_y;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArmTest.java
@@ -51,8 +51,9 @@ public class JewelArmTest extends LinearOpMode {
         jewelStick = hardwareMap.servo.get("jewelStick");
         leftJewel = hardwareMap.colorSensor.get("leftJewel");
         rightJewel = hardwareMap.colorSensor.get("rightJewel");
-
-
+        int newLeftTarget = 0;
+        int newRightTarget = 0;
+        double target = 0;
         waitForStart();
 
         jewelStick.setPosition(1);
@@ -75,11 +76,13 @@ public class JewelArmTest extends LinearOpMode {
                     telemetry.addData("left side is red", "right side is blue");
                     color = 1;
                     curr = testGyro.getHeading();
+                    target = curr+10;
                     sleep(1000);
                 } else if (leftJewel.red() + 1 < rightJewel.red() && rightJewel.blue() + 1 < leftJewel.blue()) {
                     telemetry.addData("right side is red", "left side is blue");
                     color = 2;
                     curr = testGyro.getHeading();
+                    target = curr-10;
                     sleep(1000);
 
                 } else {
@@ -88,10 +91,15 @@ public class JewelArmTest extends LinearOpMode {
 
             }
             if(color==1) {
-                double target = curr+10;
+
+                telemetry.addData("gyroC1",testGyro.getHeading());
+                telemetry.addData("target",target);
+                telemetry.addData("stage",stage);
+                telemetry.update();
+
                // if(testGyro.getHeading()<target && stage==0) {
                 if(stage<2){
-                    Motors.setP(0, 0, -(target-testGyro.getHeading())/55);
+                    Motors.setP(0, 0, -(target-testGyro.getHeading())/50);
                }
                 if(testGyro.getHeading()>=target && stage==0){
                     target=curr;
@@ -101,14 +109,19 @@ public class JewelArmTest extends LinearOpMode {
                 if(stage==1 && testGyro.getHeading()<=target+1 && testGyro.getHeading()>=target-1){
                     stage++;
                     Motors.setP(0,0,0);
+                    sleep(1000);
+                   // completed++;
                 }
 
             }
             if(color==2){
-                double target = curr-10;
+                telemetry.addData("gyroC2 ",testGyro.getHeading());
+                telemetry.addData("target",target);
+                telemetry.addData("stage",stage);
+                telemetry.update();
                 //if(testGyro.getHeading()<target && stage==0) {
                 if(stage<2) {
-                    Motors.setP(0, 0, -(target - testGyro.getHeading()) / 55);
+                    Motors.setP(0, 0, -(target - testGyro.getHeading()) / 50);
                 } //}
                 if(testGyro.getHeading()<target && stage==0){
                     target=curr;
@@ -118,13 +131,14 @@ public class JewelArmTest extends LinearOpMode {
                 if(stage==1 && testGyro.getHeading()>=target-1&& testGyro.getHeading()<=target+1){
                     stage++;
                     Motors.setP(0,0,0);
+                    sleep(1000);
+                   // completed++;
                 }
 
             }
 
         if(completed==2) {
-            int newLeftTarget = fr.getCurrentPosition() + (int) (12 * COUNTS_PER_INCH);
-            int newRightTarget = fr.getCurrentPosition() + (int) (12 * COUNTS_PER_INCH);
+
             fr.setTargetPosition(newLeftTarget);
             bl.setTargetPosition(newRightTarget);
             fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -149,6 +163,8 @@ public class JewelArmTest extends LinearOpMode {
             bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             fr.setPower(0);
             bl.setPower(0);
+             newLeftTarget = fr.getCurrentPosition() + (int) (12 * COUNTS_PER_INCH);
+            newRightTarget = fr.getCurrentPosition() + (int) (12 * COUNTS_PER_INCH);
             completed++;
             telemetry.addData("Completed1",completed);
             telemetry.update();
